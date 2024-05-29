d2c430f3c7462c2ef9cc96b6cad520dc4808723b
Nisha <nisha.saini@ongraph.com>
2024-05-29 09:44:52 +0530

    - open setting

--- app/src/main/java/com/example/baseproject/utils/Util.kt
+++ app/src/main/java/com/example/baseproject/utils/Util.kt
@@ -1,5 +1,10 @@
 package com.example.baseproject.utils
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import com.example.baseproject.BuildConfig
 import com.example.baseproject.storage.PreferenceHandler
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
@@ -15,6 +20,15 @@ object Util {
                 }
                 writeString(this.fcmToken, task.result)
             })
+    }
+
+    fun openSetting(context: Context) {
+        context.startActivity(
+            Intent(
+                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.parse("package:" + BuildConfig.APPLICATION_ID)
+            )
+        )
     }
 
 }
